6da9e612f74ac9c2dc805fc279800f9aee451bea
Sumit Yadav <ysumit2409@gmail.com>
2018-10-01 01:08:57 +0530

    change

--- validate.php
+++ validate.php
@@ -51,7 +51,7 @@ try {
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = '24yadav09sumit96@gmail.com';                 // SMTP username
-    $mail->Password = '143timu$24';                           // SMTP password
+    $mail->Password = '';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
